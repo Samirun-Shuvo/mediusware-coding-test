fbff3b9cc9dfb740614e625028a64ddb40f6aa33
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-03-25 11:34:58 +0600

    change branch

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mediusware Coading Test</h1>
+    <div>
+      <h1>Mediusware Coading Test : </h1>
     </div>
   );
 }
